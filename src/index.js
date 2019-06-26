e16acbc981b53518e3c7227c6b31b29b6e467fea
saifbinabdulkareem <saifbinabdulkarim.sk@gmail.com>
2019-06-27 00:04:09 +0500

    Redux with firebase

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './Store/Reducers/RootReducer';
 import { Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import { getFirestore } from 'redux-firestore';
-import { getFirebas} from 'react-redux-firebase'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fireConfig from './Config/FireConfig'
  
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase,getFirestore })),
+        reduxFirestore(fireConfig),
+        reactReduxFirebase(fireConfig)
+        )
+    );
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.register();
